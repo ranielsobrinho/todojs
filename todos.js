a6229743dd0ab90c315103e33570bd28e8754d03
Raniel <raniel25sobrinho@gmail.com>
2020-08-20 14:32:22 -0300

    Change only the selectors names

--- todos.js
+++ todos.js
@@ -1,7 +1,7 @@
 // Referenciando todos os elementos necessários para a nossa lista
 var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
+var inputElement = document.querySelector('#input');
+var buttonElement = document.querySelector('#btn');
 
 var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
